974eec017c01472f9090eb413a10c5c45ba05f92
Bassem Dimassi <dimassibassem99@gmail.com>
2023-01-03 21:34:30 +0100

    Clearing comments

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -2,23 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'main.dart';
 
-CollectionReference users = FirebaseFirestore.instance.collection('users');
-
-Future<void> addUser(email, password, dropDownSelectedValue, context) {
-  // Call the user's CollectionReference to add a new user
-  return users
-      .add({'email': email, 'password': password, 'role': dropDownSelectedValue})
+Future<void> addUser(email, password, dropDownSelectedValue, context) async {
+  await FirebaseFirestore.instance
+      .collection('users')
+      .add(
+          {'email': email, 'password': password, 'role': dropDownSelectedValue})
       .then((value) => Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => HomeScreen()),
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
           ))
       .catchError((error) => print("Failed to add user: $error"));
 }
 
-Future<void> registerUser(
-    String email, String password, String dropDownSelectedValue, context) async {
+Future<void> registerUser(String email, String password,
+    String dropDownSelectedValue, context) async {
   try {
-    addUser(email, password, dropDownSelectedValue, context);
+    await addUser(email, password, dropDownSelectedValue, context);
   } catch (error) {
     print(error.toString());
   }
@@ -32,7 +31,6 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreen extends State<RegisterScreen> {
-  // Define the mutable state for this widget
   String email = '';
   String password = '';
 
@@ -61,7 +59,7 @@ class _RegisterScreen extends State<RegisterScreen> {
                 height: 20,
               ),
               TextField(
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: "Email",
                   border: OutlineInputBorder(),
                 ),
@@ -89,31 +87,32 @@ class _RegisterScreen extends State<RegisterScreen> {
               const SizedBox(
                 height: 20,
               ),
-          DropdownButton<String>(
-            isExpanded: true,
-            value: _dropDownSelectedValue,
-            items: ['admin', 'user'].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String? value) {
-              setState(() {
-                _dropDownSelectedValue = value!;
-              });
-            },
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-            ),
-          ),
+              DropdownButton<String>(
+                isExpanded: true,
+                value: _dropDownSelectedValue,
+                items: ['admin', 'user'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    _dropDownSelectedValue = value!;
+                  });
+                },
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
               const SizedBox(
                 height: 10,
               ),
               ElevatedButton(
                 onPressed: () async {
-                  await registerUser(email, password, _dropDownSelectedValue, context);
+                  await registerUser(
+                      email, password, _dropDownSelectedValue, context);
                 },
                 child: const Text("Register"),
               ),
